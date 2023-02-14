7e6b16b2e8431837f97f227aec94b291b0d17480
Hardik-1168 <kavarhardik123@gmail.com>
2023-02-14 18:30:09 +0530

    devlopment

--- src/Components/Layout/CartItems.js
+++ src/Components/Layout/CartItems.js
@@ -22,20 +22,21 @@ const CartItems = () => {
   })
   let n = totalAmount.toFixed(2)
   const handlePlaceOrder = () => {
-    if(itemsList.length < 1){
+    if (itemsList.length < 1) {
       alert("Please add something in cart to place order")
-    }else{
+    } else {
       setGreet(true)
+      dispatch(cartActions.setQuantityZero())
     }
   }
-  const handleShopping = () => {
-    dispatch(cartActions.setQuantityZero())
-  }
+  // const handleShopping = () => {
+  //   dispatch(cartActions.setQuantityZero())
+  // }
   return (
     <div>
       {!greet && <div className='cartItems'>
         <h2>Your Cart</h2>
-        {cartItems.length === 0 && <h2 style={{margin:"0px"}}>Oops !! Your Cart is Empty </h2>}
+        {cartItems.length === 0 && <h2 style={{ margin: "0px" }}>Oops !! Your Cart is Empty </h2>}
 
         <ul>
           {
@@ -50,21 +51,34 @@ const CartItems = () => {
           }
         </ul>
         <div className="total">
-          {cartItems.length>=1 && <h1 style={{margin:"0px"}}>Total :${n} </h1>}
-          <button  onClick={handlePlaceOrder}>Place Order</button>
+          {cartItems.length >= 1 && <h1 style={{ margin: "0px" }}>Total :${n} </h1>}
+          <button onClick={handlePlaceOrder}>Place Order</button>
           <Link to="/products">
-          <button>Go to Shopping</button>
+            <button>Go to Shopping</button>
           </Link>
 
         </div>
-      </div> }
+      </div>}
 
-      {greet && <div className="greeting">
+      {greet && <div className='checkout-page'> <div className="greeting">
         <h1>Congratulations</h1>
         <h2>Your Order has been placed sucessfully !</h2>
-        <Link to="/">
-        <button onClick={handleShopping}>Back to Home</button>
-        </Link>
+        {
+          cartItems.map((item) => (
+            <CartItem
+              id={item.id}
+              quantity={item.quantity}
+              title={item.title}
+              price={item.price}
+              total={item.totalPrice} />
+          ))
+        }<h2>Total : ${n}</h2>
+      </div>
+        <div className="home">
+          <Link to="/">
+            <button>Back to Home</button>
+          </Link>
+        </div>
       </div>
       }
     </div>
